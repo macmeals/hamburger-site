3e4840e98e06976ea4235afba03799ff39f7304d
macmeals <68972494+macmeals@users.noreply.github.com>
2021-04-26 23:50:19 +0900

    ショップ情報のリンクを作成

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="l-footer">
    <div class ="p-footer">
-     <p>ショップ情報｜ヒストリー</p>
+     <p><a href="<?php echo home_url('/shop-info');  ?>">ショップ情報</a>｜ヒストリー</p>
      <p><small>Copyright RaiseTech</small></p>
    </div>
  </footer>
--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,9 @@
 
 <aside class="l-aside">
-<?php wp_nav_menu(array('menu'        => 'HambergerMenu',
-                        'container'   => '',
-                        'menu_class'  => 'p-aside__main',
-                        'walker'      => new hamberger_walker_side_menu )); ?>
+<?php wp_nav_menu(array('menu'        => 'HambergerMenu',     /*メニュー名を指定 */
+                        'container'   => '',                  /*containerのクラスを削除 */
+                        'menu_class'  => 'p-aside__main',     /*メインのクラス名を削除 */
+                        'walker'      => new hamberger_walker_side_menu )); ?>  <!-- サブのクラス名を記載 -->
 
     
 </aside>
